ec1ad2e02bc5114624da8ab282a49b122d78bff4
fortunat-a-ly <dubikw16@gmail.com>
2023-05-12 21:05:43 +0300

    update app key

--- src/main/java/utils/URLManager.java
+++ src/main/java/utils/URLManager.java
@@ -4,13 +4,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 public class URLManager {
     public static final String TOKEN = "";
-
     public static final String URL_UPLOAD = "https://content.dropboxapi.com/2/files/upload";
     public static final String URL_FILE_METADATA ="https://api.dropboxapi.com/2/sharing/get_file_metadata";
     public static final String URL_DELETE = "https://api.dropboxapi.com/2/file_requests/delete";
 
     /*
-    public static final String APP_KEY = "y8z4iup8xmj5me3";
+    public static final String APP_KEY = "enc1dubij92p9k1";
     public static final String URL_AUTHORIZATION = "https://www.dropbox.com/oauth2/authorize?client_id=" + APP_KEY + "&token_access_type=offline&response_type=code";
     public static final String URL_TOKEN = "https://api.dropboxapi.com/oauth2/token";
      */
